663d32d647e4e615a7fd975b4b62c3ec8f2dc5d9
Mateus Preste <mateusx.mk@gmail.com>
2021-09-27 17:03:59 -0400

    adicionando csvtest.h

---
+++ csvtest.h
@@ -0,0 +1,12 @@
+#ifndef CSVTEST   /* Include guard */
+#define CSVTEST
+
+#include <fstream>
+#include <string>
+
+
+
+//int enterBucket(std::ifstream &ifd, std::ofstream &output_file, int id, registry reg);
+//int readLine(std::ifstream &ifd, std::ofstream &output_file, std::string texto);
+
+#endif // FOO_H_
